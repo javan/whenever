2060cb95fa32c0cfaf5e8bd522df0897ba18aee2
Javan Makhmali <javan@javan.us>
2010-10-25 20:41:57 -0400

    removed base.rb

--- lib/whenever.rb
+++ lib/whenever.rb
@@ -1,10 +1,21 @@
 require 'chronic'
 require 'active_support/all'
 
-require 'whenever/base'
 require 'whenever/job_list'
 require 'whenever/job'
 require 'whenever/cron'
 require 'whenever/output_redirection'
 require 'whenever/command_line'
 require 'whenever/version'
+
+module Whenever
+  
+  def self.cron(options)
+    Whenever::JobList.new(options).generate_cron_output
+  end
+  
+  def self.path
+    Dir.pwd
+  end
+
+end
