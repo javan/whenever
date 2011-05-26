3254eea6c855d8979aa755de2e68b5d0e6cc1fb4
Andrey Chernih <andrey.chernih@gmail.com>
2011-05-26 10:38:17 +0400

    defer capistrano roles

--- lib/whenever/capistrano.rb
+++ lib/whenever/capistrano.rb
@@ -16,14 +16,14 @@ Capistrano::Configuration.instance(:must_exist).load do
   
   namespace :whenever do
     desc "Update application's crontab entries using Whenever"
-    task :update_crontab, :roles => whenever_roles do
+    task :update_crontab, :roles => lambda { whenever_roles } do
       # Hack by Jamis to skip a task if the role has no servers defined. http://tinyurl.com/ckjgnz
       next if find_servers_for_task(current_task).empty?
       run "cd #{current_path} && #{whenever_command} #{whenever_update_flags}"
     end
 
     desc "Clear application's crontab entries using Whenever"
-    task :clear_crontab, :roles => whenever_roles do
+    task :clear_crontab, :roles => lambda { whenever_roles } do
       next if find_servers_for_task(current_task).empty?
       run "cd #{release_path} && #{whenever_command} #{whenever_clear_flags}"
     end
