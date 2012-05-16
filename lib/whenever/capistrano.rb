7ae1009c31deb03c5db4a68f5fc99ea099ce5655
Michal Wrobel <sparrovv@gmail.com>
2012-05-16 16:37:42 +0300

    Use deploy:finalize_update instead deploy:restart

--- lib/whenever/capistrano.rb
+++ lib/whenever/capistrano.rb
@@ -5,7 +5,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   # Disable cron jobs at the begining of a deploy.
   after "deploy:update_code", "whenever:clear_crontab"
   # Write the new cron jobs near the end.
-  before "deploy:restart", "whenever:update_crontab"
+  before "deploy:finalize_update", "whenever:update_crontab"
   # If anything goes wrong, undo.
   after "deploy:rollback", "whenever:update_crontab"
 
