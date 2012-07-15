8a9375317670aa32cb141bc9c328c965e432e0e4
Sam Giffney <samuelgiffney@gmail.com>
2012-07-15 13:09:42 +1000

    clear crontab before finalize_update but after update_code as the whenever gem may not be available until the bundle is installed

--- lib/whenever/capistrano.rb
+++ lib/whenever/capistrano.rb
@@ -3,7 +3,7 @@ require "whenever/capistrano/recipes"
 Capistrano::Configuration.instance(:must_exist).load do
 
   # Disable cron jobs at the begining of a deploy.
-  before "deploy:update_code", "whenever:clear_crontab"
+  before "deploy:finalize_update", "whenever:clear_crontab"
   # Write the new cron jobs near the end.
   after "deploy:finalize_update", "whenever:update_crontab"
   # If anything goes wrong, undo.
