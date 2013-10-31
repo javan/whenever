a9093524ae6b11b843f59ddd83fd214bc2ead18e
Philip Hallstrom <philip@pjkh.com>
2013-10-31 10:36:49 -0700

    lose require_relative in order to support ruby 1.8

--- lib/whenever/capistrano.rb
+++ lib/whenever/capistrano.rb
@@ -1,5 +1,5 @@
 if defined?(Capistrano::VERSION) && Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new('3.0.0')
   load File.expand_path("../tasks/whenever.rake", __FILE__)
 else
-  require_relative 'capistrano2'
+  require 'whenever/capistrano2'
 end
