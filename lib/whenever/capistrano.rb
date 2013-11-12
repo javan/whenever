83be4c51352c0c7262bc7b147bbc4fda8d80074d
Philip Hallstrom <philip@pjkh.com>
2013-11-12 12:29:16 -0800

    load capistrano/version so we can test it.

--- lib/whenever/capistrano.rb
+++ lib/whenever/capistrano.rb
@@ -1,3 +1,4 @@
+require 'capistrano/version'
 if defined?(Capistrano::VERSION) && Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new('3.0.0')
   load File.expand_path("../tasks/whenever.rake", __FILE__)
 else
