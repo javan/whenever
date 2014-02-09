7adc7ad331ceb0a51bbc0a51e8e229e40b43f242
Javan Makhmali <javan@javan.us>
2014-02-09 15:51:31 -0500

    Use variables to setup job_types

--- lib/whenever/setup.rb
+++ lib/whenever/setup.rb
@@ -9,20 +9,7 @@ set :path, Whenever.path
 # http://blog.scoutapp.com/articles/2010/09/07/rvm-and-cron-in-production
 set :job_template, "/bin/bash -l -c ':job'"
 
-job_type :command, ":task :output"
-
-# Run rake through bundler if possible
-if Whenever.bundler?
-  job_type :rake, "cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
-  job_type :script, "cd :path && :environment_variable=:environment bundle exec script/:task :output"
-else
-  job_type :rake, "cd :path && :environment_variable=:environment rake :task --silent :output"
-  job_type :script, "cd :path && :environment_variable=:environment script/:task :output"
-end
-
-# Create a runner job that's appropriate for the Rails version
-def runner_for_app
-  case
+set :runner_command, case
   when Whenever.bin_rails?
     "bin/rails runner"
   when Whenever.script_rails?
@@ -30,6 +17,10 @@ def runner_for_app
   else
     "script/runner"
   end
-end
 
-job_type :runner, "cd :path && #{runner_for_app} -e :environment ':task' :output"
+set :bundle_command, Whenever.bundler? ? "bundle exec" : ""
+
+job_type :command, ":task :output"
+job_type :rake,    "cd :path && :environment_variable=:environment :bundle_command rake :task --silent :output"
+job_type :script,  "cd :path && :environment_variable=:environment :bundle_command script/:task :output"
+job_type :runner,  "cd :path && :runner_command -e :environment ':task' :output"
