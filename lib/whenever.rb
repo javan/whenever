c7f42fd34f8584858bc5406ac92c4c418fe8120f
Leonard Garvey <lengarvey@gmail.com>
2014-06-22 01:11:28 +1000

    require AS String#starts_with?

--- lib/whenever.rb
+++ lib/whenever.rb
@@ -4,6 +4,7 @@ require 'active_support/core_ext/kernel/singleton_class'
 require 'active_support/core_ext/array/wrap'
 require 'active_support/core_ext/numeric/time'
 require 'active_support/core_ext/string/filters'
+require 'active_support/core_ext/string/starts_ends_with'
 
 module Whenever
   autoload :JobList,           'whenever/job_list'
